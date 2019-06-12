036aa26440e0ce5d7ed2401eeca04f0cadece3e9
ramoncinp <ramon.parra@chipred.com>
2019-06-12 09:11:18 -0500

    Definir servicios

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
+#include <EEPROM.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
-#include <EEPROM.h>
 
 #define LED 2
 #define LED_DELAY 250
@@ -15,7 +16,8 @@ char APssid[32] = {0}; //Arreglo para almacenar AP_SSID
 String ssid = "";
 String pass = "";
 String nfc = "";
-String key = "";
+String llave = "";
+uint32_t chipId = 0;
 unsigned int udpLocalPort = 2401;
 unsigned long timeRef;
 
@@ -23,10 +25,13 @@ unsigned long timeRef;
 WiFiUDP udp;
 
 //Declaracion de funciones
+int handleRequest(String request);
 String getMemoryData();
 String saveMemoryData(String data);
 void handleUDP();
 void initWiFi();
+void prepareMemoryData();
+void setMemoryData(String data);
 
 //Funcion SETUP
 void setup() 
@@ -45,8 +50,10 @@ void setup()
 
   //Leer memoria
   String readData = getMemoryData();
-  Serial.print("Datos leidos -> ");
-  Serial.println(readData);
+  if (readData != "")
+  {
+    setMemoryData(readData);
+  }
 }
 
 //Funcion Loop
@@ -65,7 +72,129 @@ void loop()
   yield();
 }
 
+
+
 /******* DEFINICIÓN DE FUNCIONES *******/
+
+/*** Servicios ***
+ * 
+ * Configurar dispositivo:
+ * Guarda la red a la que se conecta el dispositivo
+ * Valida que la llave sea valida, si no lo es,
+ * no se guardan los datos
+ * {
+ *    "key":"config_wifi",
+ *    "llave":"2803269",
+ *    "data":{
+ *        "ssid":"AP_OFICINA",
+ *        "pass":"B1n4r1uM"
+ *    }
+ * }
+ * 
+ * Definir llave
+ * {
+ *    "key":"set_llave",
+ *    "llave":"2803269",
+ *    "data":{
+ *        "llave":"AAAA000000A00",
+ *    }
+ * }
+ * 
+ * Definir tag NFC
+ * {
+ *    "key":"set_nfc",
+ *    "llave":"2803269",
+ *    "data":{
+ *        "nfc":"29F4AD71",
+ *    }
+ * }
+ * 
+ * Abrir cerradura
+ * {
+ *    "key":"unlock",
+ *    "device_id":"2803269",
+ *    "llave":"2803269"
+ * }
+ * 
+ * Borrar memoria
+ * {
+ *    "key":"erase",
+ *    "llave":"2803269"
+ * }
+ * 
+ * ******/
+
+int handleRequest(String request)
+{
+  //Intentar hacer JSONParse
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(request);
+
+  //Validar conversión
+  if (root.success())
+  {
+    //Validar llave
+    String inLlave = root["llave"];
+    if (llave != inLlave)
+    {
+      //Error de autenticacion
+      return 1;
+    } 
+
+    //Validar que exista "key"
+    String key = root["key"];
+    if (!key)
+    {
+      return -1;
+    }
+
+    //Evaluar key
+    if (key == "unlock" || key == "erase")
+    {
+      if (key == "unlock")
+      {
+        //Nada aun :)
+      }
+    }
+    else
+    {
+      //Obtener "data"
+      String data = root["data"];
+      if (!data)
+      {
+        return -1;
+      }
+
+      if (key == "set_llave")
+      {
+        String mLlave = root["data"]["llave"];
+        llave = mLlave;
+      }
+      else if (key == "set_nfc")
+      {
+        String mNfc = root["data"]["nfc"];
+        nfc = mNfc;
+      }
+      else if (key == "config_wifi")
+      {
+        String mSsid = root["data"]["ssid"];
+        String mPass = root["data"]["pass"];
+        ssid = mSsid;
+        pass = mPass;
+      }  
+
+      //Guardar cambios
+      prepareMemoryData();  
+    }
+  }
+  else
+  {
+    return -1;
+  }
+
+  //Éxito
+  return 0;
+}
 
 /** Datos a almacenar en fomato JSON
  * 
@@ -107,6 +236,9 @@ String getMemoryData()
     if (readChar == 0)
     {
       //Si es nulo... retornar cadena leída
+      Serial.print("Datos leidos -> ");
+      Serial.println(data);
+
       return data;
     }
     else
@@ -138,22 +270,6 @@ String saveMemoryData(String data)
   EEPROM.commit();
 }
 
-/*** Servicios ***
- * 
- * Configurar dispositivo:
- * Guarda la red a la que se conecta el dispositivo
- * Valida que la llave sea valida, si no lo es,
- * no se guardan los datos
- * 
- * {
- *    "ssid":"AP_OFICINA",
- *    "pass":"B1n4r1uM",
- *    "llave":"2803269"
- * }
- * 
- * 
- * ******/
-
 void handleUDP()
 {
   //Buffer para almacenar respuesta 
@@ -183,25 +299,36 @@ void handleUDP()
       if (data.indexOf("COPACETIC") != -1)
       {
         //Es mensaje para buscar dispositivo
-        //Enviar una respuesta
+        //Enviar respuesta
         udp.beginPacket(udp.remoteIP(), udp.remotePort());
-        //udp.write((uint8_t *)&replyBuffer, strlen(replyBuffer));
         udp.write(APssid);
       }
       else
       {
-        //Guardar informacion
-        saveMemoryData(data);
+        //Evaluar request
+        int result = handleRequest(data);
 
-        //Es un mensaje JSON
-        String response = "{\"response\":\"ok\", \"message\":\"Configuraciones WiFi recibidas\"}";
-        
-        //Enviar una respuesta
+        String response = "";
+        if (result == 0)
+        {
+          response = "{\"response\":\"ok\", \"message\":\"Configuraciones WiFi recibidas\"}";
+        }
+        else if (result == -1)
+        {
+          response = "{\"response\":\"error\", \"message\":\"Error al procesar informacion\"}";
+        }
+        else if (result == 1)
+        {
+          response = "{\"response\":\"error\", \"message\":\"Error de autenticacion\"}";
+        }
+
+        //Enviar respuesta
         udp.beginPacket(udp.remoteIP(), udp.remotePort());
         udp.write(response.c_str(), strlen(response.c_str()));
       }
     }
 
+    //Cerrar paquete
     udp.endPacket();
   }
 }
@@ -213,9 +340,10 @@ void initWiFi()
   WiFi.softAPdisconnect();
 
   //Obtener chipID
-  uint32_t chipId = ESP.getChipId();
+  chipId = ESP.getChipId();
+
   //Asignarlo a variable global para autenticacion
-  key = String(chipId);
+  llave = String(chipId);
 
   //Asignar nombre de dispositivo 
   sprintf(APssid, "%s_%d", AP_NAME, chipId);
@@ -225,4 +353,56 @@ void initWiFi()
 
   //Inicializar UDP
   udp.begin(udpLocalPort);
+}
+
+/** Datos a almacenar en fomato JSON
+ * 
+ * {
+ *    "ssid":"AP_OFICINA",
+ *    "pass":"B1n4r1uM",
+ *    "llave":"2803269",
+ *    "nfc":"29F4AD71"
+ * }
+ * 
+ * */
+void prepareMemoryData()
+{
+  //Crear objeto JSON
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();  
+
+  //Agregar datos a objeto JSON
+  root["ssid"] = ssid;
+  root["pass"] = pass;
+  root["llave"] = llave;
+  root["nfc"] = nfc;
+
+  //Convertir objeto JSON a String
+  String output;
+  root.prettyPrintTo(output);
+
+  Serial.print("Almacenar en memoria -> ");
+  Serial.println(output);
+
+  //Guardar en memoria
+  saveMemoryData(output);
+}
+
+void setMemoryData(String data)
+{
+  //Intentar hacer JSONParse
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(data);
+
+  //Obtener datos
+  String mSsid = root["ssid"];
+  String mPass = root["pass"];
+  String mNfc = root["nfc"];
+  String mLlave = root["llave"];
+
+  //Asignar a variables globales
+  ssid = mSsid;
+  pass = mPass;
+  nfc = mNfc;
+  llave = mLlave;
 }
